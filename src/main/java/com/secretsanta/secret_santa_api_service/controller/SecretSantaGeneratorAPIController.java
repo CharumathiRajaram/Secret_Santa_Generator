64622ca28d93b3779eb7fe8d414c1db8fb1928ae
charumathir <charumathir@buddi.ai>
2025-03-18 14:18:26 +0530

    Initial commit - Spring Boot Secret Santa backend

---
+++ src/main/java/com/secretsanta/secret_santa_api_service/controller/SecretSantaGeneratorAPIController.java
@@ -0,0 +1,8 @@
+package com.secretsanta.secret_santa_api_service.controller;
+
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/secret_santa")
+public class SecretSantaGeneratorAPIController {
+
+}
---
+++ src/test/java/com/secretsanta/secret_santa_api_service/SecretSantaApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretsanta.secret_santa_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSantaApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
