63b5462b3fdb8b7162f33132f64360661ec54357
charumathir <charumathir@buddi.ai>
2025-03-18 17:41:08 +0530

    feat: add Assignment logic implementation

--- src/main/java/com/acme/secret/santa/service/SecretSantaAssignmentService.java
+++ src/main/java/com/acme/secret/santa/service/SecretSantaAssignmentService.java
@@ -5,12 +5,35 @@ import com.acme.secret.santa.model.Employee;
 import com.acme.secret.santa.model.SecretSantaAssignment;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class SecretSantaAssignmentService {
-    public List<SecretSantaAssignment> assignSecretSanta(List<Employee> employees,List<SecretSantaAssignment> prevAssignment){
-        //todo perform logic
-        return List.of(new SecretSantaAssignment("","","",""));
+    public List<SecretSantaAssignment> assignSecretSanta(List<Employee> employees, List<SecretSantaAssignment> prevAssignment) {
+        List<Employee> shuffled = new ArrayList<>(employees);
+        //shuffle employees
+        Collections.shuffle(shuffled);
+        Map<String, Employee> assignment = new HashMap<>();
+        for (Employee santa : employees) {
+            List<Employee> eligibleChildren = new ArrayList<>();
+            for (Employee child : shuffled) {
+                //employee should not assign themselves
+                //previous year secret child should not repeat for the same parent
+                if (!santa.equals(child) && prevAssignment.stream().noneMatch(x -> x.employeeEmail().equals(santa.employeeEmail()) && x.secretChildEmail().equals(child.employeeEmail()))) {
+                    eligibleChildren.add(child);
+                }
+            }
+            if (eligibleChildren.isEmpty()) return assignSecretSanta(employees, prevAssignment);
+            Employee assignChild = eligibleChildren.get(new Random().nextInt(eligibleChildren.size()));
+            assignment.put(santa.employeeEmail(), assignChild);
+            //remove assigned child
+            shuffled.remove(assignChild);
+        }
+        List<SecretSantaAssignment> secretSantaAssignments = new ArrayList<>();
+        for (Employee santa : employees) {
+            Employee assignChild = assignment.get(santa.employeeEmail());
+            secretSantaAssignments.add(new SecretSantaAssignment(santa.employeeName(), santa.employeeEmail(), assignChild.employeeName(), assignChild.employeeEmail()));
+        }
+        return secretSantaAssignments;
     }
 }
